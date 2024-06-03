0eab98b19a90502f453e9900e028073ba8197c02
xiaojun zhu <xiaojun.zhu@kellogg.northwestern.edu>
2024-06-03 01:11:59 +0000

    authentication done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,25 @@ class SessionsController < ApplicationController
   def new
   end
 
+
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        redirect_to "/places"
+      else
+        redirect_to "/login"
+      end
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to "/login"
   end
+
 end
+
+
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+get("/login", { :controller => "sessions", :action => "new" })
+get("/logout", { :controller => "sessions", :action => "destroy" })
+
+get("/", { :controller => "places", :action => "index" })
+
+
 end
